bdfe821e1f0cd447d95e9af0e87ec7a60ec087b1
kburovetska <kburovetska@ukr.net>
2019-02-15 11:35:30 +0200

    Added regexes for login, home and cell phones

--- src/controller/RegexCont.java
+++ src/controller/RegexCont.java
@@ -3,7 +3,10 @@ package controller;
 public interface RegexCont {
 
     String REGEX_NAME = "[А-ЯІЇ]{1}[а-яїі]+(-?[А-ЯІЇ]{1}[а-яїі]+)?";
-    String REGEX_SURNAME = "([а-яА-ЯїЇіІ]{3}\\s{1})?([а-яА-ЯїЇіІ]{1}')?[А-ЯІЇ]{1}[а-яїі]+(-?[А-ЯІЇ]{1}[а-яїі]+)?";
+    String REGEX_SURNAME = "([а-яА-ЯїЇіІ]{3}\\s{1}){0,2}([а-яА-ЯїЇіІ]{1}')?[А-ЯІЇ]{1}[а-яїі]+(-?[А-ЯІЇ]{1}[а-яїі]+)?";
     String REGEX_PATRONYMIC = "[А-ЯІЇ]{1}[а-яїі]+";
+    String REGEX_LOGIN = "[a-zA-Z]{1}\\w{3,15}";
+    String REGEX_HOME_PHONE = "[0-9]{2}\\s?[0-9]{3}\\s?[0-9]{2}\\s?[0-9]{2}";
+    String REGEX_CELL_PHONE = "(\\+?[0-9]{2})?[-\\s]?[0-9]{3}[-\\s]?[0-9]{3}[-\\s]?[0-9]{2}[-\\s]?[-\\s]?[0-9]{2}[-\\s]?";
 
 }
--- src/view/TextConsts.java
+++ src/view/TextConsts.java
@@ -5,6 +5,10 @@ public interface TextConsts {
     String INPUT_NAME = "Введіть ім'я:";
     String INPUT_SURNAME = "Введіть прізвище:";
     String INPUT_PATRONYMIC = "Введіть по-батькові:";
+    String INPUT_LOGIN = "Введіть логін:";
+    String INPUT_HOME_PHONE = "Введіть домашній телефон:";
+    String INPUT_CELL_PHONE = "Введіть мобільний телефон:";
+
     String ERROR_INPUT = "Помилка вхідних даних!";
 
 }
